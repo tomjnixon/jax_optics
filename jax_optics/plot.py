584be5d35469fbe748438e083f01b248cc3ca5f1
Thomas Nixon <tom@tomn.co.uk>
2025-01-25 01:13:15 +0000

    support plotting nested arrays of rays

--- jax_optics/plot.py
+++ jax_optics/plot.py
@@ -1,14 +1,21 @@
 import jax
+import jax.numpy as jnp
 import matplotlib.pyplot as plt
 
 
 def plot_rays(positions: jax.Array, ax=None, **kwargs):
-    """plot rays from a matrix of positions, which should be shape (n, k, 3)
-    for n rays with k positions each (for k-1 surfaces)"""
+    """plot rays from a matrix of positions, which should be shape (..., k, 3)
+    for an arbitrary structure of rays with k positions each (for k-1 surfaces)
+    """
     if ax is None:
         ax = plt.gca()
 
-    ax.plot(positions[:, :, 0].T, positions[:, :, 1].T, **kwargs)
+    *_rest, k, dim = positions.shape
+    assert dim == 3
+
+    pos_flat = jnp.reshape(positions, (-1, k, dim))
+
+    ax.plot(pos_flat[..., 0].T, pos_flat[..., 1].T, **kwargs)
 
 
 def plot_surface(surface, ax=None, n=50, **kwargs):
